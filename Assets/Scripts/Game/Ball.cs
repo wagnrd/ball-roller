52ea37e08a8b7fdfe01b5ac4ca74226d7140f7c3
wagnrd <denis@wagnrd.de>
2022-11-28 19:09:23 +0100

    adds basic level

---
+++ Assets/Scripts/Game/Ball.cs
@@ -0,0 +1,18 @@
+#nullable enable
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
